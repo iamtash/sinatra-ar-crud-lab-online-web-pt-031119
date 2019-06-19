e1332bc2cf027aa8fccf04c29668ae2dfb4c205a
Natasha Chernis <natashachernis@gmail.com>
2019-06-19 18:55:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.create(title: params[:title], content: params[:content])
     @articles = Article.all
     erb :index
-    redirect to "/articles/#{params[:id]}"
+    redirect to "/articles/#{@article.id}"
   end
 
   get '/articles' do
