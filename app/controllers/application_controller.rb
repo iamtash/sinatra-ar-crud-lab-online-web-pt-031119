a2cb9aa64c02b815d8e58a194b3e959f18a34a4b
Natasha Chernis <natashachernis@gmail.com>
2019-06-19 18:33:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    @article = Article.create(title: params[:title]), content: params[:content])
+    Article.create(title: params[:title]), content: params[:content])
     #@article = Article.find(params[:id])
     redirect '/articles/:id'
   end
